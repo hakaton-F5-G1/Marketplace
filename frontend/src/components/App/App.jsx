c5fdea739078c8ece7d2fb0e9d0093ae5957c58a
ourainbows <casasjuanfelipe@gmail.com>
2022-02-16 05:43:34 -0500

    Remove files and creates folder for components

---
+++ frontend/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { Catalog } from '../Catalog/Catalog';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Catalog/>
+    </div>
+  );
+}
+
+export default App;
