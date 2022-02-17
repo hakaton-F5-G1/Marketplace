af38c4d2a475c7f679639345cb23fd0c57ebbb75
Caio Da Silva <caiodasilvatx@gmail.com>
2022-02-17 12:35:37 +0100

    new product card

--- frontend/src/components/Product/Product.jsx
+++ frontend/src/components/Product/Product.jsx
@@ -1,23 +1,38 @@
 import { Link } from "react-router-dom"
 import "./products.css"
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 function Product({ product }) {
-    const { name, image, price, id } = product
-    return (
-        <div className="product">
-            <div className="container-img">
-                <img className="img" src={image} alt="producto" />
-            </div>
-            <span className="container-info">
-                <p className="container-name">{name}</p>
-                <span className="container-subinfo">
-                    <p className="container-info--price">{price}</p>
-                    <Link to={`product/${id}`}>Ver detalles</Link>
-                </span>
-                
-            </span>
-        </div>
-    )
+    const { id, name, image, price, description } = product
+  return (
+    <Link to={`product/${id}`}>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt="product-img"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {price}$
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </Link>
+  );
 }
-
 export { Product }
